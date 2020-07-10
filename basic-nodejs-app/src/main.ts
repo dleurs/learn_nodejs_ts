e83fb73d8231fca68a6631c4fc099396b9b62f9e
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-10 13:19:02 +0200

    Updates on basic-nodejs-app

--- basic-nodejs-app/src/main.ts
+++ basic-nodejs-app/src/main.ts
@@ -10,9 +10,9 @@ app.get('/', (req, res, _) => // _ = next
   res.send('<h1>Hello World!</h1>');
 });
 
-//const hostname: string = process.env.HOST_ADDR || "0.0.0.0";
 const port: string = process.env.PORT || "8080";
 const nodeEnv: string = process.env.NODE_ENV || "development";
+
 app.listen(parseInt(port), function () 
 {
   console.log(`Server running at http://localhost:${port}/ in ${nodeEnv}`);
